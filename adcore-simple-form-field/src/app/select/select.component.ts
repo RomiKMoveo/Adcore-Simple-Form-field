75604b4491cbf71f22b9ac58af10c1dd9fb6e8e4
romi kamin <romik@moveo.co.il>
2024-08-11 10:18:29 +0300

    added suffix and prefix from material ui

--- adcore-simple-form-field/src/app/select/select.component.ts
+++ adcore-simple-form-field/src/app/select/select.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+/**
+ * @title Basic select
+ */
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './select.component.html',
-  styleUrl: './select.component.scss'
+  styleUrl: './select.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class SelectComponent {
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
